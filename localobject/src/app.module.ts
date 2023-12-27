3f46fd3c0ee1d7c4e70b4a1e707797fce7410142
Gaston Ariel Fauret Caceres <gastonfauret@gmail.com>
2023-12-27 20:15:04 -0300

    Second Commit, Desktop Commit

--- localobject/src/app.module.ts
+++ localobject/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
 
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController],
+  controllers: [ProductsController],
   providers: [AppService],
 })
 export class AppModule {}
--- localobject/src/products/products.controller.ts
+++ localobject/src/products/products.controller.ts
@@ -1,17 +1,19 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Put, Res, Patch, Delete, Query, ParseIntPipe } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
+
+    // Metodos o peticiones http-------------------------------------------------------------------------
 
     @Get()
     getProducts(): string {
         return 'Main Page'
     }
 
-    @Get('hot')
-    getHelloProducts(): string {
-        return 'Secundary Page'
-    }
+    // @Get('hot')
+    // getHelloProducts(): string {
+    //     return 'Secundary Page'
+    // }
 
     // @Get(':id')
     // find(@Param('id') id: number) {
@@ -23,17 +25,35 @@ export class ProductsController {
     //     return `On this page we get the id ${id} and the size of the product ${size}`
     // }
 
-    @Get(':id')
-    find(@Res() response, @Param('id') id: number) {
-        if(id < 100) {
-            return response.status(HttpStatus.OK).send(`Page product ${id}`); 
-        } else { return response.status(HttpStatus.NOT_FOUND).send('Product not found.')}
-    }
+    // @Get(':id')
+    // find(@Res() response, @Param('id') id: number) {
+    //     if(id < 100) {
+    //         return response.status(HttpStatus.OK).send(`Page product ${id}`); 
+    //     } else { return response.status(HttpStatus.NOT_FOUND).send('Product not found.')}
+    // }
 
-    @Post()
-    @HttpCode(HttpStatus.NOT_FOUND)
-    createProduct(@Body('name') name: string,
-    @Body('description') description: string) {
-        return `The Product "${name}" with description "${description}" was created sucessfully`
-    }
+    // @Post()
+    // @HttpCode(HttpStatus.NOT_FOUND)
+    // createProduct(@Body('name') name: string,
+    // @Body('description') description: string) {
+    //     return `The Product "${name}" with description "${description}" was created sucessfully`
+    // }
+
+    // @Put(':id')
+    // update(@Param('id') id: number, @Body() body) {
+    //     return `You're doing a update operation of the register "${id}", with "${body.name}" and "${body.description}"`
+    // }
+
+    // @Patch(':id')
+    // modify(@Param('id') id: number, @Body() body) {
+    //     return `You have modified the item number ${id}`
+    // }
+
+    // @Delete(':id')
+    // @HttpCode(HttpStatus.NO_CONTENT)
+    // delete(@Param('id') id:number) {
+    //     return `The item with th id ${id} has been deleted`
+    // }   
+
+   
 };
